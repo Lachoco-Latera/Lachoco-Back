d10611b2f4509afbca3d5b3f9607a3c2b0921596
Carlos Molina <105681853+Crlziito04@users.noreply.github.com>
2024-06-18 23:30:35 -0300

    crud users, swagger

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,47 @@
+import { Order } from 'src/order/entities/order.entity';
+import { Product } from 'src/product/entities/product.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+export enum Role {
+  SUPERADMIN = 'SUPERADMIN',
+  ADMIN = 'ADMIN',
+  CLIENT = 'CLIENT',
+}
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuid();
+
+  @Column({ type: 'varchar', length: 40, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 40, nullable: false })
+  lastname: string;
+
+  @Column({ type: 'varchar', length: 40, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  password: string;
+
+  @Column({ type: 'enum', enum: Role, default: Role.CLIENT })
+  role: Role;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @OneToMany(() => Order, (order) => order.user)
+  @JoinColumn({ name: 'orders_id' })
+  orders: Order[];
+
+  @OneToMany(() => Product, (product) => product.user)
+  favoriteProducts: Product[];
+}
